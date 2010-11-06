8b97009147a6c5c5001d33619ac61e95acf1a356
derekthompson <derek.westlibrary@gmail.com>
2010-11-05 21:20:53 -0500

    Added test framework.  Revealed some problems.  Including test framework and updated AI.  Test framework simply fills player's role and displays situations were player wins.  Ran it on 1000 game loops until it stopped revealing flaws.  Then ran it 100000 times.  No wins.  Looks good. Should have built test framework first. Cheers.

--- models.py
+++ models.py
@@ -20,45 +20,52 @@ class Board(models.Model):
 		
 		#first try to win
 		
+		#first correction - the outer layer needed to each be if not elif
+		
 		if oneone.iso:
 			if onetwo.iso and onethree.isempty: onethree.CompClicked()
-			elif onethree.iso and onetwo.isempty: onetwo.CompClicked
+			elif onethree.iso and onetwo.isempty: onetwo.CompClicked()
 			elif twotwo.iso and threethree.isempty: threethree.CompClicked()
 			elif threethree.iso and twotwo.isempty: twotwo.CompClicked()
 			elif twoone.iso and threeone.isempty: threeone.CompClicked()
 			elif threeone.iso and twoone.isempty: twoone.CompClicked()
-		elif onetwo.iso:
-			if oneone.iso and onethree.isempty: onethree.CompClicked()
-			elif onethree.iso and oneone.isempty: oneone.CompClicked
+		if onetwo.iso:
+			if onethree.iso and oneone.isempty: oneone.CompClicked
 			elif twotwo.iso and threetwo.isempty: threetwo.CompClicked()
 			elif threetwo.iso and twotwo.isempty: twotwo.CompClicked()
-		elif onethree.iso:
+		if onethree.iso:
 			if twotwo.iso and threeone.isempty: threeone.CompClicked()
 			elif threeone.iso and twotwo.isempty: twotwo.CompClicked()
 			elif twothree.iso and threethree.isempty: threethree.CompClicked()
 			elif threethree.iso and twothree.isempty: twothree.CompClicked()
-		elif twoone.iso:
+		if twoone.iso:
 			if twotwo.iso and twothree.isempty: twothree.CompClicked()
 			elif twothree.iso and twotwo.isempty: twotwo.CompClicked()
-		elif twothree.iso:
-			if onethree.iso and threethree.isempty: threethree.CompClicked()
-			elif threethree.iso and twothree.isempty: twothree.CompClicked()
-			elif twotwo.iso and twoone.isempty: twoone.CompClicked()
-			elif twoone.isempty and twotwo.iso: twotwo.CompClicked()
-		elif threeone.iso:
+			elif threeone.iso and oneone.isempty: oneone.CompClicked()
+		if twothree.iso:
+			if twotwo.iso and twoone.isempty: twoone.CompClicked()
+			elif threethree.iso and onethree.isempty: onethree.CompClicked()
+		if threeone.iso:
 			if threetwo.iso and threethree.isempty: threethree.CompClicked()
 			elif threethree.iso and threetwo.isempty: threetwo.CompClicked()
+			elif twotwo.iso and onethree.isempty: onethree.CompClicked()
+		if threetwo.iso:
+			if twotwo.iso and onetwo.isempty: onetwo.CompClicked()
+			elif threethree.iso and threeone.isempty: threeone.CompClicked()
+		if threethree.iso:
+			if twotwo.iso and oneone.isempty: oneone.CompClicked()
 		
 		# have we won?
 		
 		if oneone.iso and ((onetwo.iso and onethree.iso) or (twotwo.iso and threethree.iso) or (twoone.iso and threeone.iso)): self.Win()
 		if onethree.iso and ((twotwo.iso and threeone.iso) or (twothree.iso and threethree.iso)): self.Win()
-		if twotwo.iso and twoone.iso and twothree.iso: self.Win()
-		if onetwo.iso and twotwo.iso and threetwo.iso: self.Win()
+		if twotwo.iso and ((twoone.iso and twothree.iso) or (onetwo.iso and threetwo.iso)): self.Win()
 		if threeone.iso and threetwo.iso and threethree.iso: self.Win()	
 		
 		if self.compwin: return True
 		# then react to player
+		
+		#outer check
 		
 		# if player holds center
 		
@@ -84,15 +91,22 @@ class Board(models.Model):
 		
 		# if computer holds center
 		
+		# second correction - needed to add extra set of brackets around some of the conditionals
+		
 		elif twotwo.iso:
-			if (oneone.isx and onetwo.isx) or (twothree.isx and threethree.isx) and onethree.isempty: onethree.CompClicked()
+			if ((oneone.isx and onetwo.isx) or (twothree.isx and threethree.isx)) and onethree.isempty: onethree.CompClicked()
 			elif oneone.isx and onethree.isx and onetwo.isempty: onetwo.CompClicked()
-			elif (oneone.isx and twoone.isx) or (threetwo.isx and threethree.isx) and threeone.isempty: threeone.CompClicked()
+			elif ((oneone.isx and twoone.isx) or (threetwo.isx and threethree.isx)) and threeone.isempty: threeone.CompClicked()
 			elif oneone.isx and threeone.isx and twoone.isempty: twoone.CompClicked()
 			elif onethree.isx and threethree.isx and twothree.isempty: twothree.CompClicked()
-			elif (onetwo.isx and onethree.isx) or (twoone.isx and threeone.isx) and oneone.isempty: oneone.CompClicked()
+			
+			#third correction - this check was missing
+			elif onethree.isx and twothree.isx and twothree.isempty: threethree.CompClicked()
+			
+			
+			elif ((onetwo.isx and onethree.isx) or (twoone.isx and threeone.isx)) and oneone.isempty: oneone.CompClicked()
 			elif threeone.isx and threethree.isx and threetwo.isempty: threetwo.CompClicked()
-			elif (threeone.isx and threetwo.isx) or (onethree.isx and twothree.isx) and threethree.isempty: threethree.CompClicked()
+			elif ((threeone.isx and threetwo.isx) or (onethree.isx and twothree.isx)) and threethree.isempty: threethree.CompClicked()
 			elif oneone.isx and threethree.isx and onetwo.isempty: onetwo.CompClicked()
 			elif oneone.isempty: oneone.CompClicked()
 			elif onethree.isempty: onethree.CompClicked()
@@ -110,7 +124,7 @@ class Board(models.Model):
 	def Win(self):
 		self.compwin=True
 		self.save()
-		return HttpResponseRedirect('/tictactoe/')
+		return HttpResponseRedirect('/tttoe/')
 	
 
 	
@@ -127,10 +141,11 @@ class Space(models.Model):
 		self.isx=True
 		self.iso=False
 		self.save()
+		return True
 	
 	def CompClicked(self):
 		self.isempty=False
 		self.isx=False
 		self.iso=True
 		self.save()
-		return HttpResponseRedirect('/tictactoe/')
+		return True
--- tests.py
+++ tests.py
@@ -1,23 +1,86 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
 
-Replace these with more appropriate tests for your application.
-"""
 
-from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from mysite.tttoe.models import Space, Board
+from django.template import RequestContext
+import random
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+def BuildSquares():
+	squares=[]
+	for each in Space.objects.all():
+		if each.iso: squares.append('O')
+		elif each.isx: squares.append('X')
+		else: squares.append('empty')
+	return squares
 
->>> 1 + 1 == 2
-True
-"""}
+	
+def index(request):
+	results=[]
+	for n in xrange(0,100):
+		moves=''
+		squares=BuildSquares()
+		new=Board()
+		new.save()
+		for x in xrange(1,4):
+			for y in xrange(1,4):
+				newspace=Space(game=new, posx=x, posy=y)
+				newspace.save()
+		while True:
+			value=False
+			for each in Board.objects.all(): 
+				if each.compwin==True: 
+					value=True
+					endgame()
+			if value: break		
+			counter=0
+			for each in Space.objects.all():
+				if each.isempty: counter+=1
+			if counter==0:
+				if checkwin(): 
+					if moves not in results: results.append(moves)
+				endgame()
+				break
+			while True:
+				ran1=random.randint(1,3)
+				ran2=random.randint(1,3)
+				check=Space.objects.get(posx=ran1, posy=ran2)
+				if check.isempty: 
+					check.PlayerClicked()
+					moves+='P'+' '+str(ran1)+str(ran2)+' '
+					break
+			for each in Board.objects.all():
+				each.Turn()
+	return render_to_response('tttoe/test.html', {'wins':results})
 
+	
+def endgame():
+	#will completely clear both database tables
+	for each in Space.objects.all(): each.delete()
+	for each in Board.objects.all(): each.delete()
+	return True
+	
+def checkwin():
+	
+	#finished board
+	
+	oneone=Space.objects.get(posx=1, posy=1)
+	onetwo=Space.objects.get(posx=1, posy=2)
+	onethree=Space.objects.get(posx=1, posy=3)
+	twoone=Space.objects.get(posx=2, posy=1)
+	twotwo=Space.objects.get(posx=2, posy=2)
+	twothree=Space.objects.get(posx=2, posy=3)
+	threeone=Space.objects.get(posx=3, posy=1)
+	threetwo=Space.objects.get(posx=3, posy=2)
+	threethree=Space.objects.get(posx=3, posy=3)
+
+	#did player beat computer
+	
+	if oneone.isx and ((onetwo.isx and onethree.isx) or (twotwo.isx and threethree.isx) or (twoone.isx and threeone.isx)): return True
+	if onethree.isx and ((twotwo.isx and threeone.isx) or (twothree.isx and threethree.isx)): return True
+	if twotwo.isx and twoone.isx and twothree.isx: return True
+	if onetwo.isx and twotwo.isx and threetwo.isx: return True
+	if threeone.isx and threetwo.isx and threethree.isx: return True
+		
+	return False
