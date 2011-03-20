06b2d7c90bb3ee1cbe6de3a77d68f5185dce6a04
Richard Bronosky <richard.bronosky@cmgdigital.com>
2011-03-19 22:54:32 -0400

    Basic list logic in place.
    BUGS:
    if len(taken_moves) == 1 & taken_moves[0] > 5, best_move will be (0, 15-taken_move)
    if taken_moves == [7,1], best_move will be (7,1)

---
+++ logic.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import itertools
+
+def move_player(player_attr):
+    def set_any(self, value):
+        try:
+            self.board[self.board.index(value)] = 0
+            setattr(self, player_attr, getattr(self, player_attr)+[value])
+        except ValueError: raise ValueError, "That position is not available on the board."
+    return set_any
+
+class magic_square(object):
+    """
+    The winning combinations of the game called tic-tac-toe add up to the magic
+    constant of a http://en.wikipedia.org/wiki/Magic_Square
+
+    The arrangement of the board is not important. All that matters is that all
+    winning combinations add up to 15. Allowing the magic square values to be
+    indexed by their tic-tac-toe position is just the UI most people expect.
+    """
+
+    player1 = property(fset=move_player('_player1'), fget=lambda self: self._player1)
+    player2 = property(fset=move_player('_player2'), fget=lambda self: self._player2)
+
+    def __init__(self):
+        self._player1, self._player2, self.board = [], [], [8, 1, 6, 3, 5, 7, 4, 9, 2]
+
+    def get_win(self, combo, available):
+        return [move for move in available if sum(combo, move) ==15 ]
+
+    def _best_move(self, combo, available):
+        return self.get_win(combo, available) + [combo]
+
+    def best_move(self, taken_moves):
+        if len(taken_moves) > 1:
+            for taken_combo in itertools.combinations(taken_moves, 2):
+                move = self._best_move(taken_combo, self.board)[0]
+        if not 'move' in locals():
+            for available_combo in itertools.combinations(self.board, 2):
+                test = self._best_move(available_combo, taken_moves)
+                if len(test) > 1: return test[1]
+        return move
+
+if __name__ == '__main__':
+    game = magic_square()
+
+    #game.player1 = 3
+    game.player1 = 8
+    game.player1 = 5
+    #game.player1 = 6
+    print 'board 1: ', game.board
+    print 'player 1: ', game.player1
+    print game.best_move(game.player1)
+
+
+"""
+def every_combo(list):
+    out = []
+    for i in list[0:-1]:
+        for e in list[1:]:
+            out.append((list[0], e))
+        list = list[1:]
+    return out
+print every_combo([1,2,3,4,5,6,7,8,9])
+print [c for c in itertools.combinations([1,2,3,4,5,6,7,8,9], 2)]
+"""
