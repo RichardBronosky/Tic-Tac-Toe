3bc8d5b58eac2e7fecee53a1716546dfdedca2ef
Richard Bronosky <richard.bronosky@cmgdigital.com>
2011-03-21 01:11:50 -0400

    Logic only beatable when cornered by a double win.

--- logic.py
+++ logic.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 
-import itertools
+import collections, itertools, random, logging
 
 def move_player(player_attr):
     def set_any(self, value):
         try:
-            attr = getattr(self, player_attr)
-            for k, v in self.board.iteritems():
-                if v == value:
-                    break
-            del self.board[k]
-            attr.update({k:v})
-            setattr(self, player_attr, attr)
+            del self.board[self.board.index(value)]
+            setattr(self, player_attr, getattr(self, player_attr)+[value])
         except ValueError: raise ValueError, "That position is not available on the board."
     return set_any
 
@@ -31,41 +26,56 @@ class magic_square(object):
         indexed by their tic-tac-toe position is just the UI most people expect.
         """
 
-        self.board,self._player1, self._player2 = dict(zip(range(1,10),
-                                                             [8, 1, 6,
-                                                              3, 5, 7,
-                                                              4, 9, 2])), {}, {}
+        self._player1, self._player2, self.board = [], [], [8, 1, 6,
+                                                            3, 5, 7,
+                                                            4, 9, 2]
 
     def get_win(self, combo, available):
         return [move for move in available if sum(combo, move) == 15]
 
-    def _best_move(self, combo, available):
-        return self.get_win(combo, available) + [combo]
-
-    def best_move(self, taken_moves):
-        if len(taken_moves) > 1:
-            for taken_combo in itertools.combinations(taken_moves.values(), 2):
-                move = self._best_move(taken_combo, self.board.values())[0]
-        if not 'move' in locals():
-            for available_combo in itertools.combinations(self.board.values(), 2):
-                test = self._best_move(available_combo, taken_moves.values())
-                if len(test) > 1: return test[1]
-        return move
+    def best_moves(self, taken_moves, opponent_moves=[], moves=[]):
+        for tactic in (taken_moves, opponent_moves):
+            # 1st go for the win
+            # 2nd block your opponent
+            if len(tactic) > 1:
+                for combo in itertools.combinations(tactic, 2):
+                    moves = moves + self.get_win(combo, self.board)
+        if len(self.board) > 1:
+            # 3rd look for combos
+            for available_combo in itertools.combinations(self.board, 2):
+                if self.get_win(available_combo, taken_moves): moves = moves + [random.choice(list(available_combo))]
+        elif len(self.board):
+            moves = moves + [random.choice(self.board)]
+        else: raise IndexError, 'game over'
+        return moves or [random.choice(self.board)]
+        # below fails when 5,8,6,4,2 are all taken
+        #return moves or [random.choice(list(set([5,8,6,4,2]) & set(self.board)))]
 
 if __name__ == '__main__':
-    game = magic_square()
+    def show(board):
+        for k in range(0,len(board)):
+            if board[k] in getattr(game, players[0]):
+                board[k] = 'X'
+            elif board[k] in getattr(game, players[1]):
+                board[k] = 'O'
+        print '-----------'
+        print '| {0}  {1}  {2} |'.format(*board[0:3])
+        print '| {0}  {1}  {2} |'.format(*board[3:6])
+        print '| {0}  {1}  {2} |'.format(*board[6:9])
+        print '-----------'
 
-    game.player1 = 5
-    game.player2 = 8
-    print 'board 1: ', game.board
-    print 'player 1: ', game.player1
-    print 'player 2: ', game.player2
-    print 'best_move 1: ', game.best_move(game.player1)
-    print 'best_move 2: ', game.best_move(game.player2)
-    game.player1 = 4
-    print 'player 1: ', game.player1
-    print 'best_move 1: ', game.best_move(game.player1)
-
+    logging.getLogger().setLevel(logging.DEBUG)
+    players = ['player1', 'player2']
+    player_queue = collections.deque(players)
+    for match in range(30):
+        game = magic_square()
+        board = game.board[:]
+        show(board)
+        while len(game.board):
+            move1 = game.best_moves(getattr(game, player_queue[0]), getattr(game, player_queue[1])) 
+            setattr(game, player_queue[0], move1[0])
+            player_queue.rotate()
+            show(board)
 
 """
 def every_combo(list):
