c66c2e3d8087afd1cbe58c18a01792f2d543c7ee
Richard Bronosky <richard.bronosky@cmgdigital.com>
2011-03-21 20:21:31 -0400

    Unbeatable logic. Clean up some redundant code. More cleanup needed. Needs a proper player class.

--- logic.py
+++ logic.py
@@ -6,8 +6,10 @@ def move_player(player_attr):
     def set_any(self, value):
         try:
             del self.board[self.board.index(value)]
-            setattr(self, player_attr, getattr(self, player_attr)+[value])
-        except ValueError: raise ValueError, "That position is not available on the board."
+            moves = getattr(self, player_attr) + [value]
+            setattr(self, player_attr, moves)
+            if len(moves) > 2 and [player_attr for combo in itertools.combinations(moves, 3) if sum(combo) == 15]: self.winner = player_attr
+        except ValueError: raise ValueError, ("Position %s is not available on the board." % value)
     return set_any
 
 class magic_square(object):
@@ -26,30 +28,33 @@ class magic_square(object):
         indexed by their tic-tac-toe position is just the UI most people expect.
         """
 
-        self._player1, self._player2, self.board = [], [], [8, 1, 6,
-                                                            3, 5, 7,
-                                                            4, 9, 2]
+        self.winner, self._player1, self._player2, self.board = None, [], [], [8, 1, 6,
+                                                                               3, 5, 7,
+                                                                               4, 9, 2]
 
     def get_win(self, combo, available):
         return [move for move in available if sum(combo, move) == 15]
 
     def best_moves(self, taken_moves, opponent_moves=[], moves=[]):
         for tactic in (taken_moves, opponent_moves):
-            # 1st go for the win
-            # 2nd block your opponent
+            # 1st tactic, go for the win. 2nd tactic, block your opponent.
             if len(tactic) > 1:
                 for combo in itertools.combinations(tactic, 2):
                     moves = moves + self.get_win(combo, self.board)
         if len(self.board) > 1:
-            # 3rd look for combos
+            # 3rd look for opponent combo intersections that would position them to win 2 ways
+            crafty = [available_combo for available_combo in itertools.combinations(self.board, 2) if self.get_win(available_combo, opponent_moves)]
+            if len(crafty) == 2: moves = moves + list(set(crafty[0]) & set(crafty[1]))
+            # 4th look for combos
             for available_combo in itertools.combinations(self.board, 2):
                 if self.get_win(available_combo, taken_moves): moves = moves + [random.choice(list(available_combo))]
         elif len(self.board):
             moves = moves + [random.choice(self.board)]
         else: raise IndexError, 'game over'
-        return moves or [random.choice(self.board)]
+        #return moves or [random.choice(self.board)]
         # below fails when 5,8,6,4,2 are all taken
         #return moves or [random.choice(list(set([5,8,6,4,2]) & set(self.board)))]
+        return moves or list(set([5]) & set(self.board)) or [random.choice(list(set([5,8,6,4,2]) & set(self.board)))] or [random.choice(self.board)]
 
 if __name__ == '__main__':
     def show(board):
@@ -71,20 +76,9 @@ if __name__ == '__main__':
         game = magic_square()
         board = game.board[:]
         show(board)
-        while len(game.board):
+        while len(game.board) and not game.winner:
             move1 = game.best_moves(getattr(game, player_queue[0]), getattr(game, player_queue[1])) 
             setattr(game, player_queue[0], move1[0])
             player_queue.rotate()
-            show(board)
-
-"""
-def every_combo(list):
-    out = []
-    for i in list[0:-1]:
-        for e in list[1:]:
-            out.append((list[0], e))
-        list = list[1:]
-    return out
-print every_combo([1,2,3,4,5,6,7,8,9])
-print [c for c in itertools.combinations([1,2,3,4,5,6,7,8,9], 2)]
-"""
+        show(board)
+        print 'winner: ', game.winner
